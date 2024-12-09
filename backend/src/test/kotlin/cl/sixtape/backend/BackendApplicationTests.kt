e2199197ce365ce748a687461305b39818ef936b
Vicente Olivares <vicente.olivares@ug.uchile.cl>
2024-12-09 01:26:51 -0300

    build(Backend): Initialize Kotlin Springboot project

---
+++ backend/src/test/kotlin/cl/sixtape/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package cl.sixtape.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
