1c4d9187881ad0e8fb4b13522b3d723687d45e0d
Vicente Olivares <vicente.olivares@ug.uchile.cl>
2025-09-19 02:02:52 -0300

    test: add test for health check endpoint

--- backend/src/test/kotlin/ApplicationTest.kt
+++ backend/src/test/kotlin/ApplicationTest.kt
@@ -5,12 +5,25 @@ import cl.sixtape.model.PostgresMovieRepository
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.content
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.Application
 import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ApplicationTest {
+    @Test
+    fun testHealthCheck() = testApplication {
+        application {
+            configureRouting()
+        }
+        val response = client.get("/healthcheck")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Healthy", response.bodyAsText())
+    }
+
     @Test
     fun newMoviesCanBeAdded() = testApplication {
         application {
